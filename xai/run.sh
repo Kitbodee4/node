5113b5a033ca10f1613f604361d58498548af52e
Kitbodee4 <99731272+Kitbodee4@users.noreply.github.com>
2023-12-08 17:37:12 +0700

    Update run.sh

--- xai/run.sh
+++ xai/run.sh
@@ -6,7 +6,7 @@ sudo apt install curl unzip
 sudo apt upgrade -y
 curl -L -0 -o sentry-node-cli-linux.zip https://github.com/xai-foundation/sentry/releases/latest/download/sentry-node-cli-linux.zip
 
-unzip sentry-node-cli-linux.zip
+unzip -o sentry-node-cli-linux.zip
 
 screen -S sentry-node-session -dm bash -c "./sentry-node-cli-linux"
 
