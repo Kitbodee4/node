68bdf642f40c6cfd5b0193380d6bb3d08a35a26a
Kitbodee4 <99731272+Kitbodee4@users.noreply.github.com>
2023-12-08 07:07:04 +0700

    Update run.sh

--- xai/run.sh
+++ xai/run.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+screen -S sentry-node-session -dm bash -c "./sentry-node-cli-linux"
 
+# Wait for a moment to ensure the screen session is ready
+sleep 2
+
+# Send 'boot-operator' command to the screen session
+screen -S sentry-node-session -X stuff "boot-operator$(printf \\r)"
+
+# Reattach to the screen session
+screen -r sentry-node-session
